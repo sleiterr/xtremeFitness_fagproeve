dc371a132a055d66f5a3407fc8d8051868eb6e91
Oleg Troian <sleite4r@gmail.com>
2026-03-10 18:26:31 +0100

    feat(about/services): refactor sections, update images and layout

--- xtreme_fitness_materials/src/components/Button/CtaLabel.jsx
+++ xtreme_fitness_materials/src/components/Button/CtaLabel.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import clsx from "clsx";
 import { MdPlayArrow } from "react-icons/md";
 
-const CtaLabel = () => {
+const CtaLabel = ({ className }) => {
   return (
     <span
-      className="inline-flex h-7.75 w-10.5 md:h-12 md:w-16 items-center justify-center rounded-full"
+      className={clsx(
+        "inline-flex h-7.75 w-10.5 md:h-12 md:w-16 items-center justify-center rounded-full",
+        className,
+      )}
       style={{ background: "var(--gradient-price-badge)" }}
     >
       <MdPlayArrow className="text-white text-3xl md:text-5xl" />
--- xtreme_fitness_materials/src/pages/Home/Home.jsx
+++ xtreme_fitness_materials/src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Exercises from "../../components/Exercises/Exercises";
-import Services from "../../components/Services/Services";
+import About from "../../components/About/About";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Exercises />
-      <Services />
+      <About />
     </>
   );
 };

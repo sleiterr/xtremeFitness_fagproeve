b0708080fdecc0547e426c8d782d942804697c52
Oleg Troian <sleite4r@gmail.com>
2026-03-12 09:49:55 +0100

    Add LinkWrapper; update Footer, ContactFooter, LogoFooter, backend schema, styles

--- xtreme_fitness_materials/src/components/Footer/ContactFooter.jsx
+++ xtreme_fitness_materials/src/components/Footer/ContactFooter.jsx
@@ -3,29 +3,40 @@ import React from "react";
 const contactData = [
   {
     id: 1,
-    href: "mailto:info@xtremefitness.dk",
-    text: "info@xtremefitness.dk",
-    className: "font-normal text-sm md:text-base text-secondary-footer",
+    title: "Adresse:",
+    text: "Nørregade 42, 9000 Aalborg",
+    titleClass: "font-normal text-base text-primary-footer text-center",
+    textClass: "font-normal text-sm md:text-base text-tertiary-footer",
   },
   {
     id: 2,
+    title: "Email:",
+    href: "mailto:info@xtremefitness.dk",
+    text: "info@xtremefitness.dk",
+    titleClass: "font-normal text-base text-primary-footer text-center",
+    textClass: "font-normal text-sm md:text-base text-tertiary-footer",
+  },
+  {
+    id: 3,
+    title: "Telefon:",
     href: "tel:+45 99 75 16 42",
     text: "+ 45 99 75 16 42",
-    className: "font-normal text-sm md:text-base text-secondary-footer",
+    titleClass: "font-normal text-base text-primary-footer text-center",
+    textClass: "font-normal text-sm md:text-base text-tertiary-footer",
   },
 ];
 
 export const ContactFooter = () => {
   return (
     <>
-      <h4 className="font-teko font-normal text-2xl md:text-3xl">Kontakt os</h4>
-      <ul className="flex flex-col items-start lg:flex-row lg:items-center justify-center gap-2 lg:gap-6">
+      <h4 className="font-teko font-normal text-2xl md:text-3xl text-center">
+        Kontakt os
+      </h4>
+      <ul className="flex flex-col items-center lg:flex-col lg:items-end justify-center gap-2 lg:gap-6">
         {contactData.map((item) => (
-          <li key={item.id} className={item.className}>
-            <a
-              href={item.href}
-              className="footer-link font-light text-lg tracking-wide text-content"
-            >
+          <li key={item.id} className="flex flex-col md:items-end">
+            <p className={item.titleClass}>{item.title}</p>
+            <a href={item.href} className={item.textClass}>
               {item.text}
             </a>
           </li>
